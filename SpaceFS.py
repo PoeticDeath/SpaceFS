3c3180dc82293741d679f4f89c966bfced3f52da
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2022-08-23 15:26:43 -0400

    Add files via upload

--- SpaceFS.py
+++ SpaceFS.py
@@ -309,8 +309,10 @@ class SpaceFS():
         lst=self.readtable()[self.filenames.index(filename)]
         if size==None:
             s=(len(lst)-1)*self.sectorsize
-            tlst=lst[-1].split(';')
-            return s+self.sectorsize-int(tlst[2])-int(tlst[1])
+            if len(lst)!=0:
+                tlst=lst[-1].split(';')
+                return s+self.sectorsize-int(tlst[2])-int(tlst[1])
+            return s
         lst=lst[:(size+self.sectorsize-1)//self.sectorsize]
         if len(lst)==0:
             if size%self.sectorsize!=0:
