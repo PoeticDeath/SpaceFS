5b4af0a56510a1b10e02c8bb4b555a39a16ebf3f
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2022-08-27 04:13:22 -0400

    Update SpaceFS.py

--- SpaceFS.py
+++ SpaceFS.py
@@ -404,9 +404,9 @@ class SpaceFS():
                 self.disk.seek(-int(self.readtable()[self.filenames.index(filename)][d*self.sectorsize+int(tmp[1])]),2)
                 self.disk.write(odata)
         st=start-(start//self.sectorsize*self.sectorsize)
-        end=(start+len(data))//self.sectorsize
+        end=(start+len(data))//self.sectorsize+1
         data=[data[:self.sectorsize-st]]+[data[i:i+self.sectorsize] for i in range(self.sectorsize-st,len(data),self.sectorsize)]
-        for i in enumerate(self.lst[start//self.sectorsize:(start+len(data))//self.sectorsize]):
+        for i in enumerate(self.lst[start//self.sectorsize:end]):
             u=0
             if type(i[1])==str:
                 u=int(i[1].split(';')[1])
