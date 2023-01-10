76640d07d1db9d16d744c54256f75a8ccc500f5f
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2023-01-09 19:58:50 -0500

    Fixed Linux mount error.

--- FuseTran.py
+++ FuseTran.py
@@ -255,6 +255,9 @@ def main():
         bs=int(argv[4])
     except IndexError:
         bs=None
-    FUSE(FuseTran(mount,disk,bs),mount,nothreads=False,foreground=fg,allow_other=True,big_writes=True,ExactFileSystemName='SpaceFS')
+    if os.name=='nt':
+        FUSE(FuseTran(mount,disk,bs),mount,nothreads=False,foreground=fg,allow_other=True,big_writes=True,ExactFileSystemName='SpaceFS')
+    else:
+        FUSE(FuseTran(mount,disk,bs),mount,nothreads=False,foreground=fg,allow_other=True,big_writes=True,fsname='SpaceFS')
 if __name__=='__main__':
     main()
