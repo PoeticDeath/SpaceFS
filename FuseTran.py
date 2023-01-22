cc5a6fc6618a30bf4ffe9b36ca3eedd56c720d30
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2023-01-21 23:22:14 -0500

    Removed redundant locks

--- FuseTran.py
+++ FuseTran.py
@@ -5,7 +5,7 @@ import struct
 from sys import argv
 from time import sleep,time
 from SpaceFS import SpaceFS
-from threading import Thread,Lock
+from threading import Thread
 from refuse.high import FUSE,FuseOSError,Operations
 class FuseTran(Operations):
     def __init__(self,mount,disk,bs=None):
@@ -18,14 +18,12 @@ class FuseTran(Operations):
                 o.write(i.to_bytes(1,'big')+bytes(4)+b'\xff\xfe')
         self.s=SpaceFS(disk)
         self.mount=mount
-        self.rwlock=Lock()
         self.fd=0
     def init(self,path):
         Thread(target=self.autosimp,daemon=True).start()
     def autosimp(self):
         while True:
-            with self.rwlock:
-                self.s.simptable()
+            self.s.simptable()
             sleep(60)
     # Filesystem methods
     # ==================
@@ -56,34 +54,33 @@ class FuseTran(Operations):
         if path in self.s.symlinks:
             return self.getattr(self.s.symlinks[path],fh,True)
         else:
-            with self.rwlock:
-                ti=time()
-                t=[ti]*3
+            ti=time()
+            t=[ti]*3
+            try:
+                s=self.s.trunfile(path)
+                gid,uid=self.s.guids[path]
+                mode=self.s.modes[path]
+                flags=self.s.winattrs[path]
+                index=self.s.filenamesdic[path]
                 try:
-                    s=self.s.trunfile(path)
-                    gid,uid=self.s.guids[path]
-                    mode=self.s.modes[path]
-                    flags=self.s.winattrs[path]
-                    index=self.s.filenamesdic[path]
-                    try:
-                        t=[struct.unpack('!d',self.s.times[index*24:index*24+24][i:i+8])[0] for i in range(0,24,8)]
-                    except struct.error:
-                        pass
-                except ValueError:
-                    s=0
-                    if os.name=='nt':
-                        gid=uid=545
-                    else:
-                        gid=uid=1000
-                    flags=0
-                    mode=16877
-                    if path!='/':
-                        if not sym:
-                            raise FuseOSError(errno.ENOENT)
-                        mode=33206
-                if bin(mode)[2:].zfill(14)[-14]=='1':
-                    return {'st_blocks':(s+self.s.sectorsize-1)//self.s.sectorsize,'st_atime':t[0],'st_mtime':t[1],'st_ctime':t[2],'st_birthtime':t[2],'st_size':s,'st_mode':mode,'st_gid':gid,'st_uid':uid,'st_flags':flags,'st_rdev':int.from_bytes(self.s.readfile(path,0,s),'big')}
-                return {'st_blocks':(s+self.s.sectorsize-1)//self.s.sectorsize,'st_atime':t[0],'st_mtime':t[1],'st_ctime':t[2],'st_birthtime':t[2],'st_size':s,'st_mode':mode,'st_gid':gid,'st_uid':uid,'st_flags':flags}
+                    t=[struct.unpack('!d',self.s.times[index*24:index*24+24][i:i+8])[0] for i in range(0,24,8)]
+                except struct.error:
+                    pass
+            except ValueError:
+                s=0
+                if os.name=='nt':
+                    gid=uid=545
+                else:
+                    gid=uid=1000
+                flags=0
+                mode=16877
+                if path!='/':
+                    if not sym:
+                        raise FuseOSError(errno.ENOENT)
+                    mode=33206
+            if bin(mode)[2:].zfill(14)[-14]=='1':
+                return {'st_blocks':(s+self.s.sectorsize-1)//self.s.sectorsize,'st_atime':t[0],'st_mtime':t[1],'st_ctime':t[2],'st_birthtime':t[2],'st_size':s,'st_mode':mode,'st_gid':gid,'st_uid':uid,'st_flags':flags,'st_rdev':int.from_bytes(self.s.readfile(path,0,s),'big')}
+            return {'st_blocks':(s+self.s.sectorsize-1)//self.s.sectorsize,'st_atime':t[0],'st_mtime':t[1],'st_ctime':t[2],'st_birthtime':t[2],'st_size':s,'st_mode':mode,'st_gid':gid,'st_uid':uid,'st_flags':flags}
     def readdir(self,path,fh):
         c=[i for i in self.s.symlinks if (path.startswith(i+'/'))|(path==i)]
         if len(c)>0:
@@ -139,8 +136,7 @@ class FuseTran(Operations):
         return self.fd
     def statfs(self,path):
         c={}
-        with self.rwlock:
-            avail=len(self.s.findnewblock(whole=True))
+        avail=len(self.s.findnewblock(whole=True))
         if avail<0:
             avail=0
         c['f_bavail']=c['f_bfree']=c['f_favail']=avail
@@ -150,8 +146,7 @@ class FuseTran(Operations):
         c['f_namemax']=255
         return c
     def unlink(self,path):
-        with self.rwlock:
-            self.s.deletefile(path)
+        self.s.deletefile(path)
     def symlink(self,name,target):
         c=os.path.normpath(self.mount+'/'.join(name.split('/')[:-1])+'/'+target).replace(self.mount,'',1).replace('\\','/')
         if os.path.exists(c):
@@ -168,15 +163,14 @@ class FuseTran(Operations):
         if old in self.s.symlinks:
             self.s.renamefile(old,new)
         else:
-            with self.rwlock:
-                tmp=list(self.s.filenamesdic.keys())
-                if self.s.modes[old]==16877:
-                    tmp.pop(tmp.index(old))
-                if old not in tmp:
-                    for i in tmp:
-                        if i.startswith(old+'/'):
-                            self.s.renamefile(i,i.replace(old,new,1))
-                self.s.renamefile(old,new)
+            tmp=list(self.s.filenamesdic.keys())
+            if self.s.modes[old]==16877:
+                tmp.pop(tmp.index(old))
+            if old not in tmp:
+                for i in tmp:
+                    if i.startswith(old+'/'):
+                        self.s.renamefile(i,i.replace(old,new,1))
+            self.s.renamefile(old,new)
     def link(self,target,name):
         pass
     def utimens(self,path,times=[time()]*2):
@@ -189,27 +183,23 @@ class FuseTran(Operations):
         self.fd+=1
         return self.fd
     def create(self,path,mode,fi=None):
-        with self.rwlock:
-            try:
-                self.s.createfile(path,mode)
-            except FileExistsError:
-                raise FuseOSError(errno.EEXIST)
-            self.fd+=1
-            return self.fd
+        try:
+            self.s.createfile(path,mode)
+        except FileExistsError:
+            raise FuseOSError(errno.EEXIST)
+        self.fd+=1
+        return self.fd
     def read(self,path,length,offset,fh):
-        with self.rwlock:
-            try:
-                return self.s.readfile(path,offset,length)
-            except EOFError:
-                pass
+        try:
+            return self.s.readfile(path,offset,length)
+        except EOFError:
+            pass
     def write(self,path,data,offset,fh):
-        with self.rwlock:
-            if self.s.writefile(path,offset,data)==0:
-                raise FuseOSError(errno.ENOSPC)
-            return len(data)
+        if self.s.writefile(path,offset,data)==0:
+            raise FuseOSError(errno.ENOSPC)
+        return len(data)
     def truncate(self,path,length,fh=None):
-        with self.rwlock:
-            self.s.trunfile(path,length)
+        self.s.trunfile(path,length)
     def setchgtime(self,path,time):
         index=self.s.filenamesdic[path]
         self.s.times=self.s.times[:index*24+8]+struct.pack('!d',time)+self.s.times[index*24+16:]
@@ -219,8 +209,7 @@ class FuseTran(Operations):
         self.s.times=self.s.times[:index*24+16]+struct.pack('!d',time)+self.s.times[index*24+24:]
         return 0
     def destroy(self,path):
-        with self.rwlock:
-            self.s.simptable(F=True)
+        self.s.simptable(F=True)
     def flush(self,path,fh):
         return 0
     def release(self,path,fh):
