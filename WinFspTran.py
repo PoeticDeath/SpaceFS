b40a46c8be35eb0b6fe2e7692ab4d3a907a75509
Anthony Kerr <46275713+PoeticDeath@users.noreply.github.com>
2023-03-13 01:23:02 -0400

    Only truncate if too big V2

--- WinFspTran.py
+++ WinFspTran.py
@@ -182,7 +182,8 @@ class SpaceFSOperations(BaseFileSystemOperations):
         if file_context.split(':')[0][1:] not in self.s.filenamesdic:
             self.s.createfile(file_context.split(':')[0][1:],448)
             DSD=self.s.readfile(dir_name[1:],0,self.s.trunfile(dir_name[1:]))
-            self.s.trunfile(file_context.split(':')[0][1:],len(DSD))
+            if self.s.trunfile(file_context.split(':')[0][1:])>len(DSD):
+                self.s.trunfile(file_context.split(':')[0][1:],len(DSD))
             self.s.writefile(file_context.split(':')[0][1:],0,DSD)
         return SecurityDescriptor.from_string(self.s.readfile(file_context.split(':')[0][1:],0,self.s.trunfile(file_context.split(':')[0][1:])).decode())
     @operation
